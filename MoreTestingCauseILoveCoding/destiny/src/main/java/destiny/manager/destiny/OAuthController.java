a3f8c541232bb7199f10ff7dd7994e3c0a8a25a5
Adam Dickinson <adamgamesyt@gmail.com>
2023-01-31 08:02:05 +0200

    Removed UnUsed Imports
    From AccessTokenRepo and OAuthController

--- MoreTestingCauseILoveCoding/destiny/src/main/java/destiny/manager/destiny/OAuthController.java
+++ MoreTestingCauseILoveCoding/destiny/src/main/java/destiny/manager/destiny/OAuthController.java
@@ -3,14 +3,9 @@ package destiny.manager.destiny;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
